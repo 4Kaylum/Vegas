3e6a771b7f78bc0f82d219187e49228122944e68
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-23 02:40:26 +0100

    Actually change the user's account when the win/lose money

--- cogs/transaction_handler.py
+++ cogs/transaction_handler.py
@@ -16,6 +16,13 @@ class TransactionHandler(utils.Cog):
                 VALUES ($1, $2, $3, $4, $5, $6)""",
                 member.id, member.guild.id, currency, amount or None, reason, win,
             )
+            if amount and currency:
+                await db(
+                    """INSERT INTO user_money (user_id, guild_id, currency_name, money_amount) VALUES ($1, $2, $3, $4)
+                    ON CONFLICT (user_id, guild_id, currency_name) DO UPDATE SET
+                    money_amount=user_money.money_amount+excluded.money_amount""",
+                    member.id, member.guild.id, currency, amount,
+                )
 
 
 def setup(bot: utils.Bot):
