618ec9bb4fa88c4ce105e2d87ea6464f8204cfd3
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-26 01:04:03 +0100

    Fix SQL insert for daily command

--- cogs/currency_commands.py
+++ cogs/currency_commands.py
@@ -251,8 +251,8 @@ class CurrencyCommands(utils.Cog):
             for row in allowed_daily_currencies:
                 amount = random.randint(9_000, 13_000)
                 await db(
-                    """INSERT INTO user_money (user_id, guild_id, currency_name, money_amount) VALUES ($1, $2, $3, $4, $5)
-                    ON CONFLICT (user_id, guild_id, currency_name) DO UPDATE SET
+                    """INSERT INTO user_money (user_id, guild_id, currency_name, money_amount, last_daily_command)
+                    VALUES ($1, $2, $3, $4, $5) ON CONFLICT (user_id, guild_id, currency_name) DO UPDATE SET
                     money_amount=user_money.money_amount+excluded.money_amount, last_daily_command=excluded.last_daily_command""",
                     ctx.author.id, ctx.guild.id, row['currency_name'], amount, ctx.message.created_at,
                 )
