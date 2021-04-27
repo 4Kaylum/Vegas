f1bd696e6e7353ca0c263eb81adb3dc261e915dc
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-28 00:57:37 +0100

    Link to current open game if people try to bet multiple times at once

--- cogs/utils/game_lock_handler.py
+++ cogs/utils/game_lock_handler.py
@@ -6,17 +6,18 @@ from discord.ext import commands
 
 class GameLockHandler(object):
 
-    locks = collections.defaultdict(asyncio.Lock)
+    locks = collections.defaultdict(lambda: (asyncio.Lock(), None))
 
     @classmethod
     async def lock(cls, _, ctx):
-        current_lock = cls.locks[(ctx.guild.id, ctx.author.id,)]
+        current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
         if current_lock.locked:
-            raise commands.CheckFailure("You can't bet multiple times at once!")
+            raise commands.CheckFailure(f"You can't bet multiple times at once - finish your current game first! (<{bet_message.jump_url}>)")
         await current_lock.lock()
+        cls.locks[(ctx.guild.id, ctx.author.id,)] = (current_lock, ctx.message,)
 
     @classmethod
     async def unlock(cls, _, ctx):
-        current_lock = cls.locks[(ctx.guild.id, ctx.author.id,)]
+        current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
         await current_lock.unlock()
 
