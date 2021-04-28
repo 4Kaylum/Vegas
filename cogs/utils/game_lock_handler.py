cd7b88d9924e00ab0f6e4b1d1f00e70ae2e72276
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-28 01:16:41 +0100

    Change raises for if we couldn't find a message to link to for a current game

--- cogs/utils/game_lock_handler.py
+++ cogs/utils/game_lock_handler.py
@@ -6,18 +6,20 @@ from discord.ext import commands
 
 class GameLockHandler(object):
 
-    locks = collections.defaultdict(lambda: (asyncio.Lock(), None))
+    locks = {}
 
     @classmethod
     async def lock(cls, _, ctx):
-        current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
+        current_lock, bet_message = cls.locks.get((ctx.guild.id, ctx.author.id,), (asyncio.Lock(), None,))
         if current_lock.locked():
-            raise commands.CheckFailure(f"You can't bet multiple times at once - finish your current game first! (<{bet_message.jump_url}>)")
+            if bet_message:
+                raise commands.CheckFailure(f"You can't bet multiple times at once - finish your current game first! (<{bet_message.jump_url}>)")
+            raise commands.CheckFailure(f"You can't bet multiple times at once - finish your current game first! (could not find message)")
         await current_lock.acquire()
         cls.locks[(ctx.guild.id, ctx.author.id,)] = (current_lock, ctx.message,)
 
     @classmethod
     async def unlock(cls, _, ctx):
-        current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
+        current_lock, bet_message = cls.locks.get((ctx.guild.id, ctx.author.id,), (asyncio.Lock(), None,))
         current_lock.release()
         cls.locks[(ctx.guild.id, ctx.author.id,)] = (current_lock, None,)
