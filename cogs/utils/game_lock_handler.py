b158f812657c0d441f09f48aa169fc2ae48b5564
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-28 01:02:11 +0100

    Fix asyncio lock method calls

--- cogs/utils/game_lock_handler.py
+++ cogs/utils/game_lock_handler.py
@@ -13,11 +13,11 @@ class GameLockHandler(object):
         current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
         if current_lock.locked():
             raise commands.CheckFailure(f"You can't bet multiple times at once - finish your current game first! (<{bet_message.jump_url}>)")
-        await current_lock.lock()
+        await current_lock.acquire()
         cls.locks[(ctx.guild.id, ctx.author.id,)] = (current_lock, ctx.message,)
 
     @classmethod
     async def unlock(cls, _, ctx):
         current_lock, bet_message = cls.locks[(ctx.guild.id, ctx.author.id,)]
-        await current_lock.unlock()
+        current_lock.release()
 
